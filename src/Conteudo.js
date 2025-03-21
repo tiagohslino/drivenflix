3624edbf26f376965904df3877421002cb2fbe3d
Tiago <tiagohslino@gmail.com>
2025-03-21 10:27:34 -0300

    print array jsx map

--- src/Conteudo.js
+++ src/Conteudo.js
@@ -1,9 +1,30 @@
 export default function Conteudo() {
+
+    const filmes = [
+        {nome: "10 regras para salvar um casamento", imagem: "https://image.tmdb.org/t/p/w185/rcmoIeno4fh6QnNmg93TVNq8aj.jpg"},
+        {nome: "Estado elétrico", imagem: "https://image.tmdb.org/t/p/w185/mUBxeBpI5xg66YhLJxg8Ppv0ArW.jpg"},
+        {nome: "O agiota", imagem: "https://image.tmdb.org/t/p/w185/sdbAoj5akfFHj7dTDOpUIrTcHT1.jpg"},
+        {nome: "O fim da lealdade", imagem: "https://image.tmdb.org/t/p/w185/sy1r4ABRjNrk6Xsp2hPnfhDPM7.jpg"},
+        {nome: "Depravação", imagem: "https://image.tmdb.org/t/p/w185/n19vWwaN3NgxazMbxxNYn5h30EE.jpg"},
+        {nome: "Batman Ninja vs Yakuza", imagem: "https://image.tmdb.org/t/p/w185/E0Kfd0UsphezvdhLVNtOTXzOLT.jpg"},
+        {nome: "Tornado", imagem: "https://image.tmdb.org/t/p/w185/p91IMcRiHvDc1Ou8rDBdEIHZcli.jpg"},
+        {nome: "De volta à ação", imagem: "https://image.tmdb.org/t/p/w185/m4gzzzeYRmrwasrBdJmjhBmjS3U.jpg"},
+        {nome: "Oficina do diabo", imagem: "https://image.tmdb.org/t/p/w185/33ugYq5qlJTAiXkfIhdUSxQUTT8.jpg"},
+        {nome: "Demolidor", imagem: "https://image.tmdb.org/t/p/w185/eODcpeaj6jFoVG1kBVpNFyFh45I.jpg"},
+    ]
+
     return (
         <ul class="conteudo">
-            <li>Senhor dos anéis: sociedade do anel</li>    
-            <li>Senhor dos anéis 2</li>
-            <li>Senhor dos anéis 3</li>
+            {filmes.map(item => <Filme nome={item.nome} imagem={item.imagem} />)}
         </ul>  
+    )
+}
+
+function Filme(props){
+    return (
+        <li class="filme">
+            <img src={props.imagem} />
+            <p>{props.nome}</p>
+        </li>
     )
 }
--- src/Menu.js
+++ src/Menu.js
@@ -1,19 +1,19 @@
 export default function Menu() {
+
+    const menu = [
+        {texto: "Home", imagem: "https://www.seekpng.com/png/full/375-3752606_homepage-icon-house-logo-png-white.png", link: "./home.html"},
+        {texto: "Favoritos", imagem: "https://icon-library.com/images/white-heart-icon-png/white-heart-icon-png-11.jpg", link: "./favoritos.html"},
+        {texto: "Contato", imagem: "https://i.im.ge/2022/09/15/1X4f74.envelope.png", link: "./contato.html"},
+    ];
+    
     return (
         <ul class="menu">
-            <MenuItem texto="Home" imagem="https://www.seekpng.com/png/full/375-3752606_homepage-icon-house-logo-png-white.png" link="./home.html" />
-            <MenuItem texto="Favoritos" imagem="https://icon-library.com/images/white-heart-icon-png/white-heart-icon-png-11.jpg" link="./favoritos.html" />
-            <MenuItem texto="Contato" imagem="https://i.im.ge/2022/09/15/1X4f74.envelope.png" link="./contato.html" />
+            {menu.map(item => <MenuItem texto={item.texto} imagem={item.imagem} link={item.link} />)}
         </ul> 
     )
 }
 
 function MenuItem(props) {
-
-    const texto = "Home";
-    const img = "https://www.seekpng.com/png/full/375-3752606_homepage-icon-house-logo-png-white.png";
-    const link = "./home.html";
-
     return (
         <li>
             <img src={props.imagem} />
