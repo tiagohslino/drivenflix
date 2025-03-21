40dc1166753677ba921e4d5e3279089f6d7c5695
Tiago <tiagohslino@gmail.com>
2025-03-21 15:18:41 -0300

    usestate nome and avatar

--- src/Topo.js
+++ src/Topo.js
@@ -4,7 +4,7 @@ export default function Topo() {
     const imagemInicial = "https://media.istockphoto.com/id/1162440985/pt/vetorial/user-profile-icon-flat-red-round-button-vector-illustration.jpg?s=612x612&w=0&k=20&c=utyHx8WLEWEMSvhWQ4Hq0P-UFtoBJ6x7j2PwPYgQ5xg=";
     
     const [nome, setNome] = React.useState("");
-    const [avatar, setAvatar] = React.useState(imagemInicial)
+    const [avatar, setAvatar] = React.useState(imagemInicial);
     
     function inserirNome() {
         const nomeDigitado = prompt("Qual é seu nome?");
@@ -12,8 +12,8 @@ export default function Topo() {
     }
 
     function mudaImagem() {
-        linkDigitado = prompt("Digite o link da sua imagem");
-        
+        const linkDigitado = prompt("Digite o link da sua imagem");
+        setAvatar(linkDigitado);
     }
 
     return (
@@ -22,7 +22,7 @@ export default function Topo() {
             <div>
                 <button onClick={inserirNome}>Inserir nome</button>
                 <p>{!nome ? "Seja bem-vindo(a) ao site!" : `Seja bem-vindo(a), ${nome}!`}</p>
-                <img onClick={() => alert("Clicou na imagem")} height="50px" src={avatar} />
+                <img onClick={mudaImagem} height="50px" src={avatar} />
             </div>
         </div>
     )
