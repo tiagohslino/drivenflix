b25878dad16c5e8a5d8d049df9a817f2d1b0b738
Tiago <tiagohslino@gmail.com>
2025-03-21 10:56:40 -0300

    react onclick

--- src/Topo.js
+++ src/Topo.js
@@ -1,16 +1,18 @@
 export default function Topo() {
-
-    // const nome = prompt("Qual seu nome?");
-    const nome = "Usuario";
+    let nome = "";
     const imagemUsuario = "https://media.istockphoto.com/id/1162440985/pt/vetorial/user-profile-icon-flat-red-round-button-vector-illustration.jpg?s=612x612&w=0&k=20&c=utyHx8WLEWEMSvhWQ4Hq0P-UFtoBJ6x7j2PwPYgQ5xg=";
+
+    function inserirNome() {
+        nome = prompt("Qual é seu nome?");
+    }
 
     return (
         <div class="topo">
             <h1>Drivenflix</h1>
             <div>
-                <button>Inserir nome</button>
+                <button onClick={inserirNome}>Inserir nome</button>
                 <p>{!nome ? "Seja bem-vindo(a) ao site!" : `Seja bem-vindo(a), ${nome}!`}</p>
-                <img height="50px" src={imagemUsuario} />
+                <img onClick={() => alert("Clicou na imagem")} height="50px" src={imagemUsuario} />
             </div>
         </div>
     )
